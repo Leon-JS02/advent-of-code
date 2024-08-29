fa77b752fd6c9083c38d25e8d89cf1ccdef3b0bc
Leon <leonjs02@protonmail.com>
2024-08-29 14:09:44 +0100

    Solved second part.

--- 2015/day2/day2.py
+++ 2015/day2/day2.py
@@ -35,9 +35,30 @@ def load_inputs(file_name: str) -> list[str]:
         return file.readlines()
 
 
+def get_shortest_length_around_sides(dims: list[int]) -> int:
+    """Gets the length of the shortest path around the sides of the gift."""
+    dims.remove(max(dims))
+    return (2 * dims[0]) + (2 * dims[1])
+
+
+def get_length_of_ribbon(dims: list[int]) -> int:
+    """Gets the length of the ribbon needed."""
+    return dims[0] * dims[1] * dims[2]
+
+
+def calculate_total_ribbon_needed(dims_str: str) -> int:
+    """Calculates the total ribbon needed."""
+    dims = get_dimensions_from_string(dims_str)
+    return get_length_of_ribbon(dims) + get_shortest_length_around_sides(dims)
+
+
 if __name__ == "__main__":
     puzzle_inputs = load_inputs('day2_inputs.txt')
     total_paper = 0
     for line in puzzle_inputs:
         total_paper += calculate_paper_needed(line)
     print(total_paper)
+    total_ribbon = 0
+    for line in puzzle_inputs:
+        total_ribbon += calculate_total_ribbon_needed(line)
+    print(total_ribbon)
