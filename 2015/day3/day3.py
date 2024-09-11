9ec39159bc82a5bd08d39e6efbbb82bb6a22bcd4
Leon <leonjs02@protonmail.com>
2024-09-11 14:00:55 +0100

    Tested and implemented day 3.

--- 2015/day3/day3.py
+++ 2015/day3/day3.py
@@ -1,1 +1,40 @@
 """Solution to Day 3 of 2015's advent of code."""
+
+
+def load_input(filename: str) -> list[str]:
+    """Loads the puzzle inputs for the day."""
+    with open(filename, 'r', encoding='utf-8') as f:
+        line = f.readline()
+    return list(line)
+
+
+def process_direction(direction: str, current_loc: tuple[int, int]) -> tuple[int, int]:
+    """Processes a single direction command and returns the new location."""
+    x, y = current_loc
+    match direction:
+        case "^":
+            new_dir = (x, y + 1)
+        case ">":
+            new_dir = (x + 1, y)
+        case "v":
+            new_dir = (x, y - 1)
+        case "<":
+            new_dir = (x - 1, y)
+    return new_dir
+
+
+def process_direction_string(directions: list[str]) -> dict:
+    """Returns a dictionary of house location -> num visits."""
+    current_location = (0, 0)
+    visited_locations = {current_location: 1}
+    for direction in directions:
+        current_location = process_direction(direction, current_location)
+        visited_locations[current_location] = visited_locations.get(
+            current_location, 0) + 1
+    return visited_locations
+
+
+if __name__ == "__main__":
+    inputs = load_input('day3_inputs.txt')
+    house_map = process_direction_string(inputs)
+    print(f"Number of houses visited: {len(house_map)}")
--- 2015/day3/test_day3.py
+++ 2015/day3/test_day3.py
@@ -1,3 +1,33 @@
 """Tests for day3."""
 
 # pylint: skip-file
+
+import pytest
+
+from day3 import process_direction, process_direction_string
+
+
+@pytest.mark.parametrize("direction, current_loc, expected", [
+    ("^", (0, 0), (0, 1)),
+    (">", (0, 0), (1, 0)),
+    ("v", (0, 0), (0, -1)),
+    ("<", (0, 0), (-1, 0)),
+    ("^", (2, 3), (2, 4)),
+    (">", (-1, -1), (0, -1)),
+    ("v", (1, 2), (1, 1)),
+    ("<", (3, 3), (2, 3)),
+])
+def test_process_direction(direction, current_loc, expected):
+    assert process_direction(direction, current_loc) == expected
+
+
+@pytest.mark.parametrize("directions, expected", [
+    (["^"], {(0, 0): 1, (0, 1): 1}),
+    (["^", "v"], {(0, 0): 2, (0, 1): 1}),
+    (["^", "^", "^"], {(0, 0): 1, (0, 1): 1, (0, 2): 1, (0, 3): 1}),
+    ([], {(0, 0): 1}),
+    (["<", "<", "<", "<"], {(0, 0): 1, (-1, 0)
+     : 1, (-2, 0): 1, (-3, 0): 1, (-4, 0): 1}),
+])
+def test_process_direction_string(directions, expected):
+    assert process_direction_string(directions) == expected
